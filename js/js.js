e6bd30ae277aa259444b1699927f1a56632efd90
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-02-21 14:27:22 -0400

    Criada e aplicada a base do jogo

---
+++ js/js.js
@@ -0,0 +1,8 @@
+function start(){ //criada a func do inicio do ogo
+    $("#inicio").hide(); //vai ocultar a div inicio
+
+    $("#fundoGame").append("<div id ='jogador'></div>")
+    $("#fundoGame").append("<div id ='inimigo1'></div>")
+    $("#fundoGame").append("<div id ='inimigo2'></div>")
+    $("#fundoGame").append("<div id ='amigo'></div>")
+}
