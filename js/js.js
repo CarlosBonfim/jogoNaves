d144f58b72aaead7c0349468832a58f3f21c3318
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-02-22 19:00:55 -0400

    Animacoes personagens add jogador inimigo1 amigo1

--- js/js.js
+++ js/js.js
@@ -1,8 +1,8 @@
 function start(){ //criada a func do inicio do ogo
     $("#inicio").hide(); //vai ocultar a div inicio
 
-    $("#fundoGame").append("<div id ='jogador'></div>")
-    $("#fundoGame").append("<div id ='inimigo1'></div>")
-    $("#fundoGame").append("<div id ='inimigo2'></div>")
-    $("#fundoGame").append("<div id ='amigo'></div>")
+    $("#fundoGame").append("<div id ='jogador' class='anima1' ></div>")
+    $("#fundoGame").append("<div id ='inimigo1' class='anima2'></div>")
+    $("#fundoGame").append("<div id ='inimigo2' ></div>")
+    $("#fundoGame").append("<div id ='amigo' class = 'anima3'></div>")
 }
