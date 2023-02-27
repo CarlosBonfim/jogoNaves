3e8c8c6e54e4c0ff332384f8d09def23189a71ab
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-02-27 10:35:30 -0400

    add funcao de movimentacao do fundo e identacao

--- js/js.js
+++ js/js.js
@@ -1,8 +1,24 @@
-function start(){ //criada a func do inicio do ogo
+function start() {
+    //criada a func do inicio do jogo
     $("#inicio").hide(); //vai ocultar a div inicio
+    // vai adicionar os elementos na area do jogo
+    $("#fundoGame").append("<div id ='jogador' class='anima1' ></div>");
+    $("#fundoGame").append("<div id ='inimigo1' class='anima2'></div>");
+    $("#fundoGame").append("<div id ='inimigo2' ></div>");
+    $("#fundoGame").append("<div id ='amigo' class = 'anima3'></div>");
 
-    $("#fundoGame").append("<div id ='jogador' class='anima1' ></div>")
-    $("#fundoGame").append("<div id ='inimigo1' class='anima2'></div>")
-    $("#fundoGame").append("<div id ='inimigo2' ></div>")
-    $("#fundoGame").append("<div id ='amigo' class = 'anima3'></div>")
+    var jogo = {};
+
+    //loop
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+        movefundo();
+    }
+
+    // vai fazer a moviementacao do background
+    function movefundo() {
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 1);
+    }
 }
