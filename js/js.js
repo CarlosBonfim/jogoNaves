4f4feaab32806a9444093bcf40cd766e904f4fae
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-02-27 15:48:31 -0400

    criar movimentacao do jogador
    Receber as teclas, criar movimentao cima e baixo

--- js/js.js
+++ js/js.js
@@ -9,16 +9,55 @@ function start() {
 
     var jogo = {};
 
+    var tecla = {
+        W: 87,
+        S: 83,
+        D: 68,
+    };
+
+    jogo.pressionou = [];
+
+    //verificar se o jogador pressionou
+
+    $(document).keydown(function (e) {
+        jogo.pressionou[e.which] = true;
+        console.log(`apertou ${e.which}`);
+    });
+
+    $(document).keyup(function (e) {
+        jogo.pressionou[e.which] = false;
+        console.log(e.which);
+    });
+
     //loop
     jogo.timer = setInterval(loop, 30);
 
     function loop() {
         movefundo();
+        movejogador();
     }
 
     // vai fazer a moviementacao do background
     function movefundo() {
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position", esquerda - 1);
+    }
+
+    function movejogador() {
+        if (jogo.pressionou[tecla.W]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo - 10);
+            if (topo <= 0) {
+                $("#jogador").css("top", topo + 10);
+            }
+        }
+
+        if (jogo.pressionou[tecla.S]) {
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo + 10);
+            if (topo >= 434) {
+                $("#jogador").css("top", topo - 10);
+            }
+        }
     }
 }
