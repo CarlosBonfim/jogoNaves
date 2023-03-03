c4f2b2adc6b649e4d59c786c9c8bc4632e355bc8
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-03-03 15:17:20 -0400

    disparo e explosao por colisao
    helicoptero jogador vai disparar e ao bater, helicoptero inimigo vai explodir

--- js/js.js
+++ js/js.js
@@ -41,6 +41,7 @@ function start() {
         moveInimigo1();
         moveInimigo2();
         moveAmigo();
+        colisao();
     }
 
     //moviementacao do background do jogo
@@ -65,8 +66,8 @@ function start() {
                 $("#jogador").css("top", topo - 10);
             }
         }
-        if(jogo.pressionou[tecla.D]){
-            disparo()
+        if (jogo.pressionou[tecla.D]) {
+            disparo();
         }
     }
     //movimentacao do inimigo 1
@@ -101,31 +102,59 @@ function start() {
             $("#amigo").css("left", 0);
         }
     }
-    function disparo(){
-        if(podeAtirar === true){
+    //disparo helicoptero jogador
+    function disparo() {
+        if (podeAtirar === true) {
             podeAtirar = false;
-            topo = parseInt($("#jogador").css("top"))
+            topo = parseInt($("#jogador").css("top"));
             posicaoX = parseInt($("#jogador").css("left"));
             tiroX = posicaoX + 190;
             topoTiro = topo + 37;
             $("#fundoGame").append("<div id ='disparo' ></div>");
-            $("#disparo").css("top", topoTiro)
-            $("#disparo").css("left", tiroX)
+            $("#disparo").css("top", topoTiro);
+            $("#disparo").css("left", tiroX);
         }
         var tempoDisparo = window.setInterval(executaDisparo, 30);
-
-        function executaDisparo(){
-            
-            posicaoX = parseInt($("#disparo").css("left"))
-            $("#disparo").css("left", posicaoX + 10)
-
-            if(posicaoX > 900){
-                window.clearInterval(tempoDisparo)
+        //realizacao do disparo
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left", posicaoX + 10);
+            if (posicaoX > 900) {
+                window.clearInterval(tempoDisparo);
                 tempoDisparo = null;
-                $("#disparo").remove()
+                $("#disparo").remove();
                 podeAtirar = true;
             }
         }
     }
-    
+    //colisao jogador inimigo 1
+    function colisao() {
+        var colisao1 = $("#jogador").collision($("#inimigo1"));
+        if (colisao1.length > 0) {
+            inimigo1X = parseInt($("#inimigo1").css("left"));
+            inimigo1Y = parseInt($("#inimigo1").css("top"));
+            explosao1(inimigo1X, inimigo1Y);
+            console.log("chamar explosao");
+
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+        }
+    }
+    function explosao1(inimigo1X, inimigo1Y) {
+        $("#fundoGame").append("<div id='explosao1'></div>");
+        $("#explosao1").css("background-image", "url(imgs/explosao.png)");
+        var div = $("#explosao1");
+        div.css("top", inimigo1Y);
+        div.css("left", inimigo1X);
+        div.animate({ width: 200, opacity: 0 }, "slow");
+        console.log("entrou explosao 1")
+
+        var tempoExplosao = window.setInterval(removeExplosao, 1000);
+        function removeExplosao() {
+            div.remove();
+            window.clearInterval(tempoExplosao);
+            tempoExplosao = null;
+        }
+    }
 }
