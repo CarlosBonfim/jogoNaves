a2d35cd1fa1a3f0d20530eb1371670b61f69a7e4
Carlos Bruno Bonfim <33705093+CarlosBonfim@users.noreply.github.com>
2023-03-08 15:24:30 -0400

    aumentado nivel de dificuldade
    Cada vez que acertar um disparo no inimigo 1, a velocidade dele aumentara quando voltar

--- js/js.js
+++ js/js.js
@@ -165,6 +165,7 @@ function start() {
         //disparo inimigo 1
         if (colisao3.length > 0) {
             pontos = pontos + 100;
+            velocidade = velocidade + 0.3
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
             explosao1(inimigo1X, inimigo1Y);
